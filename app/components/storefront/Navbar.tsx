6f7749eb3ff68b15b5debf90d29e0a0b9b5c2e62
Moamer AL-SHOUSHY <alshoushy@gmail.com>
2024-07-18 01:58:47 +0200

    some edit in navbar'

--- app/components/storefront/Navbar.tsx
+++ app/components/storefront/Navbar.tsx
@@ -23,8 +23,8 @@ export async function Navbar() {
     <nav className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex items-center justify-between">
       <div className="flex items-center">
         <Link href="/">
-          <h1 className="text-black font-bold text-xl lg:text-3xl">
-            Shoe<span className="text-primary">Marshal</span>
+          <h1 className="text-primary font-bold text-xl lg:text-3xl">
+            E-<span className="text-black">commerce</span>
           </h1>
         </Link>
         <NavbarLinks />
